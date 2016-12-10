5a177f0944e98b633f52bf2d8cd4400fccf193c5
Jackon Yang <i@jackon.me>
2016-12-10 22:57:10 +0800

    find best post by euclidian distance

--- building-machine-learning-systems-with-python/chap03_cluster.py
+++ building-machine-learning-systems-with-python/chap03_cluster.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import sys
+
 from sklearn.feature_extraction.text import CountVectorizer
 import scipy as sp
 
@@ -16,17 +18,43 @@ x = vectorizer.fit_transform(content)
 
 num_samples, num_features = x.shape
 
-print 'samples: %s, features: %s' % (num_samples, num_features)
+# print 'samples: %s, features: %s' % (num_samples, num_features)
 
-print vectorizer.get_feature_names()
+# print vectorizer.get_feature_names()
 
 new_post = 'imaging databases'
 new_post_vec = vectorizer.transform([new_post])
 
-print new_post_vec
-print new_post_vec.toarray()
+# print new_post_vec
+# print new_post_vec.toarray()
 
 
 def dist_raw(v1, v2):
     delta = v1 - v2
     return sp.linalg.norm(delta.toarray())
+
+
+best_post = None
+best_dist = sys.maxint
+best_i = None
+
+
+for i in range(num_samples):
+    post = content[i]
+
+    if post == new_post:
+        # leave one out alg.
+        continue
+
+    post_vec = x.getrow(i)
+
+    d = dist_raw(post_vec, new_post_vec)
+
+    print '=== Post %s with dist=%.2f: %s' % (i, d, post)
+
+    if d < best_dist:
+        best_dist = d
+        best_i = i
+        best_post = post
+
+print 'Best Post is %s with dist=%.2f: %s' % (best_i, best_dist, best_post)
