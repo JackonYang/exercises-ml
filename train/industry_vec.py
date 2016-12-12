f6fd40a7de8b46e991c2d64a6bc7daa0ab70f86c
Jackon Yang <i@jackon.me>
2016-12-12 18:03:43 +0800

    load data

---
+++ train/industry_vec.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import os
+
+
+_home_path = os.path.expanduser('~')
+MLCOMP_DIR = os.path.join(_home_path, 'data')
+
+groups = [
+    'comp.graphics',
+    'comp.os.ms-windows.misc',
+    'comp.sys.ibm.pc.hardware',
+    'comp.sys.mac.hardware',
+    'comp.windows.x',
+    'sci.space',
+]
+
+
+def fetch_data(name, set_, mlcomp_root, categories=None):
+    mlcomp_root = os.path.expanduser(mlcomp_root)
+    mlcomp_root = os.path.abspath(mlcomp_root)
+    mlcomp_root = os.path.normpath(mlcomp_root)
+
+    dataset_path = os.path.join(mlcomp_root, name, set_)
+
+    folders = [f for f in sorted(os.listdir(dataset_path))
+               if os.path.isdir(os.path.join(dataset_path, f))]
+    if categories is not None:
+        folders = [f for f in folders if f in categories]
+
+    for label, folder in enumerate(folders):
+        folder_path = os.path.join(dataset_path, folder)
+
+        documents = [os.path.join(folder_path, d)
+                     for d in sorted(os.listdir(folder_path))]
+        for filename in documents:
+            with open(filename, 'rb') as f:
+                yield folder, f.read()
+
+train_data = fetch_data('379', 'train', mlcomp_root=MLCOMP_DIR, categories=groups)
+# test_data = load_data('379', 'test', mlcomp_root=MLCOMP_DIR, categories=groups)
+
+
+for target_name, data in train_data:
+    print target_name
+
+
+
+# print len(train_data.filenames)
+# print len(test_data.filenames)
